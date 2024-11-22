6178de2cfe9ff821e6684d2b8ce2c836b4c3b6bb
Gahyeon19 <wjsrkgus0119@naver.com>
2024-11-22 17:25:46 +0900

    bastion 비활성화

--- work/bastion.tf
+++ work/bastion.tf
@@ -7,7 +7,7 @@
 # }
 
 # resource "aws_instance" "sample-ec2-bastion" {
-#   ami = "ami-01711d925a1e4cc3a"
+#   ami = "ami-040c33c6a51fd5d96"
 #   instance_type = "t2.micro"
 #   vpc_security_group_ids = [aws_security_group.sample-sg-bastion.id]
 #   subnet_id = aws_subnet.sample-subnet-public-01.id
