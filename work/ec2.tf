de7c5d14994b8656632f0a93c1b853f5559790ae
Gahyeon19 <wjsrkgus0119@naver.com>
2024-11-22 17:26:12 +0900

    ec2 생성 시 nginx 자동 설치 코드 변경

--- work/ec2.tf
+++ work/ec2.tf
@@ -1,17 +1,20 @@
 resource "aws_instance" "sample-ec2-01" {
-  ami = "ami-0de20b1c8590e09c5"   #각자 생성한 ami
+  ami = "ami-040c33c6a51fd5d96"   #각자 생성한 ami
   instance_type = "t2.micro"
   key_name      = "kosta_edu"
   associate_public_ip_address = true # 퍼블릭 IP 자동 할당
   subnet_id = aws_subnet.sample-subnet-public-01.id
   vpc_security_group_ids = [aws_security_group.sample-sg-elb.id]
-  user_data = <<EOF
+  user_data = <<-EOF
 		#! /bin/bash
-    sudo apt update
-	  sudo apt install -y nginx
-    sudo systemctl start nginx
-    sudo systemctl enable nginx
-    echo "This is Test Server" > /usr/share/nginx/html/index.html
+    while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
+    sleep 1
+    done
+    apt update
+	  apt install -y nginx
+    systemctl start nginx
+    systemctl enable nginx
+    echo "This is Test Server----------1" > /var/www/html/index.nginx-debian.html
   EOF
   root_block_device {
     volume_size = "30"
@@ -26,19 +29,22 @@ resource "aws_instance" "sample-ec2-01" {
 }
 
 resource "aws_instance" "sample-ec2-02" {
-  ami = "ami-0de20b1c8590e09c5"   #각자 생성한 ami
+  ami = "ami-040c33c6a51fd5d96"   #각자 생성한 ami
   instance_type = "t2.micro"
   key_name      = "kosta_edu"
   associate_public_ip_address = true # 퍼블릭 IP 자동 할당
   subnet_id = aws_subnet.sample-subnet-public-02.id
   vpc_security_group_ids = [aws_security_group.sample-sg-elb.id]
-  user_data = <<EOF
+  user_data = <<-EOF
 		#! /bin/bash
-    sudo apt update
-	  sudo apt install -y nginx
-    sudo systemctl start nginx
-    sudo systemctl enable nginx
-    echo "This is Test Server" > /usr/share/nginx/html/index.html
+    while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
+    sleep 1
+    done
+    apt update
+	  apt install -y nginx
+    systemctl start nginx
+    systemctl enable nginx
+    echo "This is Test Server----------2" > /var/www/html/index.nginx-debian.html
   EOF
   root_block_device {
     volume_size = "30"
@@ -53,7 +59,7 @@ resource "aws_instance" "sample-ec2-02" {
 }
 
 resource "aws_instance" "sample-ec2-03" {
-  ami = "ami-0de20b1c8590e09c5" 
+  ami = "ami-040c33c6a51fd5d96" 
   instance_type = "t2.micro"
   key_name      = "kosta_edu"
   associate_public_ip_address = false   #private
