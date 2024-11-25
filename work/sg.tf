7d1cb50473a709a8d686a54ff3939ec3576316c5
Gahyeon19 <wjsrkgus0119@naver.com>
2024-11-25 14:42:32 +0900

    add sg - https 443

--- work/sg.tf
+++ work/sg.tf
@@ -60,6 +60,16 @@ resource "aws_security_group_rule" "sample-sg-elb-inbound2" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+resource "aws_security_group_rule" "sample-sg-elb-inbound3" {
+  security_group_id = aws_security_group.sample-sg-elb.id
+  type = "ingress"
+  description = "allow all for https"
+  from_port = 443 
+  to_port = 443 
+  protocol = "tcp" 
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "sample-sg-elb-egress" {
   security_group_id = aws_security_group.sample-sg-elb.id
   type = "egress"   #아웃바운드
