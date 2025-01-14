d2911ac07445407badfe33d8d048936b39d7bd6c
Gahyeon19 <wjsrkgus0119@naver.com>
2025-01-14 15:09:46 +0900

    modified terraform code

--- work/ec2.tf
+++ work/ec2.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "sample-ec2-01" {
 	  apt install -y nginx
     systemctl start nginx
     systemctl enable nginx
-    echo "This is Test Server----------1" > /var/www/html/index.nginx-debian.html
+    echo "This is Test Server -------- 1" > /var/www/html/index.nginx-debian.html
   EOF
   root_block_device {
     volume_size = "30"
@@ -44,7 +44,7 @@ resource "aws_instance" "sample-ec2-02" {
 	  apt install -y nginx
     systemctl start nginx
     systemctl enable nginx
-    echo "This is Test Server----------2" > /var/www/html/index.nginx-debian.html
+    echo "This is Test Server -------- 2" > /var/www/html/index.nginx-debian.html
   EOF
   root_block_device {
     volume_size = "30"
@@ -59,13 +59,23 @@ resource "aws_instance" "sample-ec2-02" {
 }
 
 resource "aws_instance" "sample-ec2-03" {
-  ami = "ami-040c33c6a51fd5d96" 
+  ami = "ami-040c33c6a51fd5d96"   #각자 생성한 ami
   instance_type = "t2.micro"
   key_name      = "kosta_edu"
-  associate_public_ip_address = false   #private
-  subnet_id = aws_subnet.sample-subnet-private-01.id
+  associate_public_ip_address = true # 퍼블릭 IP 자동 할당
+  subnet_id = aws_subnet.sample-subnet-public-01.id
   vpc_security_group_ids = [aws_security_group.sample-sg-elb.id]
-  
+  user_data = <<-EOF
+		#! /bin/bash
+    while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
+    sleep 1
+    done
+    apt update
+	  apt install -y nginx
+    systemctl start nginx
+    systemctl enable nginx
+    echo "This is Test Server -------- 3" > /var/www/html/index.nginx-debian.html
+  EOF
   root_block_device {
     volume_size = "30"
     volume_type = "gp3"
@@ -73,8 +83,58 @@ resource "aws_instance" "sample-ec2-03" {
       "Name" = "sample-ec2-03"
     }
   }
-
   tags = {
     Name = "sample-ec2-03"
   }
 }
+
+resource "aws_instance" "sample-ec2-04" {
+  ami = "ami-040c33c6a51fd5d96"   #각자 생성한 ami
+  instance_type = "t2.micro"
+  key_name      = "kosta_edu"
+  associate_public_ip_address = true # 퍼블릭 IP 자동 할당
+  subnet_id = aws_subnet.sample-subnet-public-02.id
+  vpc_security_group_ids = [aws_security_group.sample-sg-elb.id]
+  user_data = <<-EOF
+		#! /bin/bash
+    while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
+    sleep 1
+    done
+    apt update
+	  apt install -y nginx
+    systemctl start nginx
+    systemctl enable nginx
+    echo "This is Test Server -------- 4" > /var/www/html/index.nginx-debian.html
+  EOF
+  root_block_device {
+    volume_size = "30"
+    volume_type = "gp3"
+    tags = {
+      "Name" = "sample-ec2-04"
+    }
+  }
+  tags = {
+    Name = "sample-ec2-04"
+  }
+}
+
+# resource "aws_instance" "sample-ec2-03" {
+#   ami = "ami-040c33c6a51fd5d96" 
+#   instance_type = "t2.micro"
+#   key_name      = "kosta_edu"
+#   associate_public_ip_address = false   #private
+#   subnet_id = aws_subnet.sample-subnet-private-01.id
+#   vpc_security_group_ids = [aws_security_group.sample-sg-elb.id]
+  
+#   root_block_device {
+#     volume_size = "30"
+#     volume_type = "gp3"
+#     tags = {
+#       "Name" = "sample-ec2-03"
+#     }
+#   }
+
+#   tags = {
+#     Name = "sample-ec2-03"
+#   }
+# }
